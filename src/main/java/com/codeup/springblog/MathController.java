e9a1954fe54c1255c1d989e7c67ee0a228fe70ef
Hunter Parks <parks.hunter1992@gmail.com>
2021-06-09 12:17:44 -0500

    finished MathController methods

---
+++ src/main/java/com/codeup/springblog/MathController.java
@@ -0,0 +1,42 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MathController {
+
+    // Adding Method
+//    @GetMapping("/add/{number}/and/{number2}")
+//    @ResponseBody
+//
+//    public String math (@PathVariable int number, @PathVariable int number2) {
+//        return String.valueOf(number + number2);
+//    }
+
+    // Subtracting Method
+//    @GetMapping("/subtract/{number}/from/{number2}")
+//    @ResponseBody
+//
+//    public String math (@PathVariable int number, @PathVariable int number2) {
+//        return String.valueOf(number2 - number);
+//    }
+
+    // Multiplying Method
+//    @GetMapping("/multiply/{number}/and/{number2}")
+//    @ResponseBody
+//
+//    public String math (@PathVariable int number, @PathVariable int number2) {
+//        return String.valueOf(number * number2);
+//    }
+
+    // Dividing Method
+    @GetMapping("/divide/{number}/by/{number2}")
+    @ResponseBody
+
+    public String math (@PathVariable int number, @PathVariable int number2) {
+        return String.valueOf(number / number2);
+    }
+}
