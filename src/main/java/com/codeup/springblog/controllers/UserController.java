6e1f42549d35aaa2b779660ccd3389a588328996
Hunter Parks <parks.hunter1992@gmail.com>
2021-06-17 12:06:11 -0500

    almost done with security exercise, just need to access logged in user - going to lunch

--- src/main/java/com/codeup/springblog/controllers/UserController.java
+++ src/main/java/com/codeup/springblog/controllers/UserController.java
@@ -1,21 +1,51 @@
 package com.codeup.springblog.controllers;
 
 
+import com.codeup.springblog.models.User;
+import com.codeup.springblog.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
 
-    @GetMapping("/users/all")
-    public String getAllUsers() {
-        return "users";
+    private UserRepository users;
+    private PasswordEncoder passwordEncoder;
+
+    public UserController(UserRepository users, PasswordEncoder passwordEncoder) {
+        this.users = users;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    @GetMapping("/user")
-    public String getTheStringUsers() {
-        return "users";
+    @GetMapping("/sign-up")
+    public String showSignUpForm(Model model) {
+        model.addAttribute("user", new User());
+
+        return "users/signup";
     }
+
+    @PostMapping("/sign-up")
+    public String saveUser(@ModelAttribute User user) {
+        String hashPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hashPassword);
+        users.save(user);
+
+        return "redirect:/login";
+    }
+
+//    @GetMapping("/users/all")
+//    public String getAllUsers() {
+//        return "users";
+//    }
+//
+//    @GetMapping("/user")
+//    public String getTheStringUsers() {
+//        return "users";
+//    }
 
 }
